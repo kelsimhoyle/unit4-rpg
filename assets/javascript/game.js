9fca371fef9bb8758ac733b6baf5762cde51cadf
Kelsi H <kelsimhoyle@gmail.com>
2019-05-09 09:16:13 -0600

    generate messages during attacks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,16 +36,62 @@ var characters = [
     }
 ];
 
+
+
 // create characters to go on the screen
 function createCharacters() {
     for (var i = 0; i < characters.length; i++) {
-        var charDiv = $("<div>").attr("id", characters[i].name).addClass("character");
+        var charDiv = $("<div>").addClass("character").attr("id", characters[i].name);
+        charDiv.attr("data-attack", characters[i].attack).attr("data-hp", characters[i].HP).attr("data-counterattack", characters[i].counterAttack);
         var charPic = $("<img>").attr("src", characters[i].src);
         var charName = $("<h3>").text(characters[i].name);
         var charHP = $("<p>").text("HP: " + characters[i].HP);
         $(charDiv).append(charName).append(charPic).append(charHP);
         $("#character-choice").append(charDiv);
     }
+}
+
+function fight() {
+    // values needed in order to play
+    var userName = $(".user").attr("id");
+    var defenderName = $(".defender").attr("id");
+    var userHP = parseInt($(".user").attr("data-hp"));
+    var attack = parseInt($(".user").attr("data-attack"));
+    var defenderHP = parseInt($(".defender").attr("data-hp"));
+    var counterAttack = parseInt($(".defender").attr("data-counterattack"));
+    var messages = [
+        {
+            attackMessage: userName + " quickly attacked " +  defenderName + " with " + attack + " damage. " + defenderName + " has " + defenderHP + " left.",
+            counterMessage: defenderName + " fought back and caused " + counterAttack + " damage on " + userName + ". Leaving " + userName + " with " + userHP + " HP left."
+        },
+        {
+            attackMessage: userName + " has launched an attack! " + defenderName + " has sustained " + attack + " damage and has " + defenderHP + " HP remaining.",
+            counterMessage: defenderName + " quickly attacked back and caused " + counterAttack + " damage. " + userName + " has " + userHP + " remaining."
+        }
+    ]
+    
+    function randomAttack() {
+        return messages[Math.floor(Math.random() * messages.length)].attackMessage;
+    }
+
+    function randomCounter() {
+        return messages[Math.floor(Math.random() * messages.length)].counterMessage;
+    }
+    var roundText = $("<div>").addClass("round-text");
+    var attackText = $("<p>").addClass("attack-text");
+    var counterText = $("<p>").addClass("counter-text");
+
+    $("#attack").on("click", function() {
+        $("#messages").empty();
+        defenderHP -= attack;
+        attack += 5;
+        userHP -= counterAttack;
+        attackText.text(randomAttack());
+        counterText.text(randomCounter());
+        roundText.append(attackText).append(counterText);
+        $("#messages").append(roundText);
+
+    });
 }
 
 
@@ -62,22 +108,25 @@ function playGame() {
         if (isCharacterChosen) {
             isDefenderChosen = true;
             this.chosen = true;
-            $(this).addClass("selected");
-            $("#defender").append(this);
+            $(this).addClass("selected defender");
+            $("#defender-display").append(this);
         } else {
             this.chosen = true;
             isCharacterChosen = true;
-            $(this).addClass("selected");
-            $("#user-character").append(this);
+            $(this).addClass("selected user");
+            $("#user-display").append(this);
         }
         if (isCharacterChosen && isDefenderChosen) {
             $(".character").not(".selected").appendTo("#enemies");
             $("#enemies").show();
             $(".playing").show();
+            fight();
         }
     });
     
-    
+
+
+
 
 }
 
